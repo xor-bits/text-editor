3131ac3ee08be8456814da730a5369b417f59bdd
Eemeli Lehtonen <oneleh@utu.fi>
2025-03-10 16:02:59 +0200

    change default bg color

--- src/editor/theme.rs
+++ src/editor/theme.rs
@@ -2,7 +2,7 @@ use ratatui::style::Color;
 
 //
 
-pub const BACKGROUND: Color = Color::Rgb(0x1E, 0x1E, 0x2E);
+pub const BACKGROUND: Color = Color::Rgb(0x24, 0x27, 0x3A);
 pub const CURSOR_LINE: Color = Color::Rgb(0x2A, 0x2B, 0x3C);
 pub const CURSOR: Color = Color::Rgb(0xB4, 0xBE, 0xFE);
 pub const BUFFER_LINE: Color = Color::Rgb(0x18, 0x18, 0x25);
